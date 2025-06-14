e006d6a9a8e76ca6d40e965e1ccc6dbebd4f7e8e
AtharvaGahine11 <2024.atharvag@isu.ac.in>
2025-06-14 12:42:26 +0530

    done

--- script.js
+++ script.js
@@ -23,4 +23,33 @@ function scrollSlider(direction) {
 
   
 
-  
+  const profileToggle = document.getElementById("profileToggle");
+const profileDropdown = document.getElementById("profileDropdown");
+
+profileToggle.addEventListener("click", () => {
+  profileDropdown.style.display = profileDropdown.style.display === "block" ? "none" : "block";
+});
+
+// Optional: Hide dropdown when clicked outside
+document.addEventListener("click", function (e) {
+  if (!profileToggle.contains(e.target) && !profileDropdown.contains(e.target)) {
+    profileDropdown.style.display = "none";
+  }
+});
+
+
+
+
+const cartToggle = document.getElementById("cartToggle");
+const cartDropdown = document.getElementById("cartDropdown");
+
+cartToggle.addEventListener("click", () => {
+  cartDropdown.style.display = cartDropdown.style.display === "block" ? "none" : "block";
+});
+
+// Optional: Hide dropdown when clicked outside
+document.addEventListener("click", function (e) {
+  if (!cartToggle.contains(e.target) && !cartDropdown.contains(e.target)) {
+    cartDropdown.style.display = "none";
+  }
+});
